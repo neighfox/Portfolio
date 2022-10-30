efaf790d2925f27f70b9357c17dbed36cb9be6a7
Neigh Fox <neighfox@gmail.com>
2022-10-30 15:47:59 +0000

    Online Editing

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,8 +10,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Nathan Fox </span>
             from <span className="purple"> Bristol, U.K.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+        
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
