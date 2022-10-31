ffe87f7500f010145865e5f3b4581abf6614dc7f
Neigh Fox <neighfox@gmail.com>
2022-10-31 15:38:04 +0000

    Resume Taken Down

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -40,7 +40,7 @@ function ResumeNew() {
           </Document>
         </Row> */}
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -50,7 +50,9 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row>
+        </Row> */}    
+
+      
       </Container>
     </div>
   );
