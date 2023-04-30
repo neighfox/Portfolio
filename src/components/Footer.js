e27891118c6fa53cf80b6b4191e04cdbf4a3598c
neigh <neighfox@gmail.com>
2023-04-30 22:42:46 +0000

    nasme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Nathan Fox</h3>
+          <h3>Designed and Developed by Roscoe Fox</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} NF</h3>
+          <h3>Copyright © {year} RF</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> NATHAN FOX</strong>
+                <strong className="main-name"> Roscoe Fox</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
